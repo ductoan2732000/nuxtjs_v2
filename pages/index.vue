c50acd1c9feb0359ccfea355191582609226e7f3
Trần Toản <ductoan2732000@gmail.com>
2022-08-09 21:55:29 +0700

    delete component

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <Tutorial />
+  <b-form>
+    <b-card>
+      <b-form-group
+        id="fieldset-1"
+        label="Enter your name"
+        label-for="input-1"
+        valid-feedback="Thank you!"
+        :invalid-feedback="invalidFeedback"
+        :state="state"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="name"
+          :state="state"
+          trim
+        ></b-form-input>
+      </b-form-group>
+    </b-card>
+  </b-form>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
+export default {
   name: 'IndexPage',
-})
+}
 </script>
